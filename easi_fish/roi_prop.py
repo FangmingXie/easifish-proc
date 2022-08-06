12664d055dcbb0827e481e33998bdd75fea8e408
Fangming Xie <fmxie1993@gmail.com>
2022-08-06 10:36:31 -0700

    cut fat on roi_prop

--- easi_fish/roi_prop.py
+++ easi_fish/roi_prop.py
@@ -5,7 +5,8 @@ import numpy as np
 import pandas as pd
 from glob import glob
 from os.path import abspath, dirname
-from skimage.measure import regionprops
+from skimage.measure import regionprops, regionprops_table
+import time
 
 def roi_prop(lb,s=[0.92,0.92,0.84],ex=2):
 
@@ -32,5 +33,33 @@ def roi_prop(lb,s=[0.92,0.92,0.84],ex=2):
         df.loc[df.index[i], 'eccentricity'] = lb_stat[i].minor_axis_length/lb_stat[i].major_axis_length
         df.loc[df.index[i], 'solidity'] = lb_stat[i].solidity
         
-    return(df)
+    return df
 
+def roi_prop_v2(lb,s=[0.92,0.92,0.84],ex=2):
+
+    """
+    Returns ROI position, volume, aspect ratio.  
+    This function uses the regionprops function implemented in skimage  
+    lb: segmentation mask
+    s: pixel size in µm for [x,y,z],default [0.92,0.92,0.84], 
+    ex: linear expansion factor, default is 2
+    """
+    s0, s1, s2 = s
+    lb_id = np.unique(lb[lb != 0])
+    # roi=len(lb_id)
+    # df = pd.DataFrame(np.empty([roi, 0]))
+    # lb_stat = regionprops(lb)
+    lb_stat = regionprops_table(lb, properties=('centroid',
+                                                 'area',
+                                                #  'major_axis_length',
+                                                #  'minor_axis_length',
+                                                #  'solidity',
+                                                ))
+    lb_stat = pd.DataFrame(lb_stat)
+    lb_stat['roi'] = lb_id
+    lb_stat['z'] = lb_stat['centroid-0']*(s2/ex)
+    lb_stat['y'] = lb_stat['centroid-1']*(s1/ex)
+    lb_stat['x'] = lb_stat['centroid-2']*(s0/ex)
+    lb_stat['area'] = lb_stat['area']*(s2*s1*s0/ex**3)
+  
+    return lb_stat[['roi', 'z', 'y', 'x', 'area']]
